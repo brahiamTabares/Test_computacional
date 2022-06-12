70f58ead0f4602fef709e1684e24d15fdbb2bdd1
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2022-06-12 16:54:30 -0500

    Se arregla la excepcion usuario duplicado

--- include/GuardarUsuario.php
+++ include/GuardarUsuario.php
@@ -1,21 +1,46 @@
 <?php
 include 'db.php';
 
-      $conn=conectar();
 
-             if (isset($_POST['guardar_usuario'])) {
+/**
+ * @throws Exception
+ */
+       $conn=conectar();
+       if (isset($_POST['guardar_usuario'])) {
 
-                 $nombre = $_POST['nombre'];
-                 session_id($_POST['cedula']);
-                 session_start();
-                 $id = session_id();
-
-                 $query = "INSERT INTO usuario(`id`, `nombre`) VALUES ('$id', '$nombre');";
-                 mysqli_query($conn, $query);
-             }
+           $nombre = $_POST['nombre'];
+           session_id($_POST['cedula']);
+           session_start();
+           $id = session_id();
+           $bandera=false;
 
 
-      desconectar($conn);
-header("Location: http://localhost/php_test_computacional/GUI/ejercicios/ejercicio1.html");
+           $consulta = "select id from usuario;";
+
+           if ($resultado = mysqli_query($conn, $consulta)) {
+
+               /* obtener el array asociativo */
+               while ($fila = mysqli_fetch_row($resultado)) {
+                   if($id==$fila[0]){
+                        $bandera=true;
+                     break;
+                   }
+               }
+
+           }
+           if($bandera){
+
+               header("Location: http://localhost/php_test_computacional/GUI/");
+           }else{
+               $query = "INSERT INTO usuario(`id`, `nombre`) VALUES ('$id', '$nombre');";
+               mysqli_query($conn, $query);
+               header("Location: http://localhost/php_test_computacional/GUI/ejercicios/ejercicio1.html");
+           }
+           desconectar($conn);
+
+
+
+       }
+
 exit();
 ?>
