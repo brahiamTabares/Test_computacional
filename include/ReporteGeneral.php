86f139c7e44abccfeb89c3899a0bd05cb525b411
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2022-06-11 22:26:04 -0500

    "Se elimina archivo encuesta y se agrega etiqueta html al reporte general"

--- include/ReporteGeneral.php
+++ include/ReporteGeneral.php
@@ -14,7 +14,19 @@ printf ("%s (%s)\n", $fila[0], $fila[1]);
     mysqli_free_result($resultado);
 }
 ?>
-
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    
+    </body>
+    </html>
 
 <?php
 // respuestas correctas e incorrectas por ejercicio
