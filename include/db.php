36de29a3633979532b0ab810cae651657e6e8a72
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2022-06-12 09:32:14 -0500

    se agrega la exepecion de la bases de datos no conectada

--- include/db.php
+++ include/db.php
@@ -1,13 +1,25 @@
 <?php
+/**
+ * @throws mysqli_sql_exception
+ */
+
 // we connect to localhost:80
+
 function conectar (){
     $conn = mysqli_connect("localhost", "root", "root", "test_computacional");
     if (!isset($conn)) {
-/// error
-        echo "no se conecto";
+
+        throw  new mysqli_sql_exception("no se conecto a la base de datos");
     }
 
     return $conn;
+}
+
+try {
+     conectar();
+
+} catch (mysqli_sql_exception $e) {
+    echo 'No se pudo conectar a la base de datos: ',  $e->getMessage(), "\n";
 }
 
 function desconectar($conn){
